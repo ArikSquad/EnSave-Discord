4c8e7a2025fefbd053a8ceee7f23e9e0fff03f39
Ari <artturi.kuula2@gmail.com>
2021-09-26 11:52:35 +0300

    Added hangman!

--- cogs/games.py
+++ cogs/games.py
@@ -3,12 +3,74 @@ import random
 import discord
 from discord.ext import commands
 
+from game import hangman
+
 
 class Games(commands.Cog, description="Game commands"):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(name='hangman', aliases=['hang'])
+    async def hangman(self, ctx):
+        """Play Hangman"""
+        await hangman.play(self.bot, ctx)
+
+    """
+    
+    # NEED TO BE FIXED
+    
+    @commands.command(name='rps', aliases=['rockpaperscissors'])
+    async def rps(self, ctx):
+        
+
+        def check_win(p, b):
+            if p == '🌑':
+                return False if b == '📄' else True
+            if p == '📄':
+                return False if b == '✂' else True
+            # p=='✂'
+            return False if b == '🪨' else True
+
+        async with ctx.typing():
+            reactions = ['🪨', '📄', '✂']
+            game_message = await ctx.send("**Rock Paper Scissors**\nChoose your shape:", delete_after=15.0)
+            for reaction in reactions:
+                await game_message.add_reaction(reaction)
+            bot_emoji = random.choice(reactions)
+
+        def check(reaction, user):
+            return user != self.bot.user and user == ctx.author and (str(reaction.emoji) == '🪨' or '📄' or '✂')
+
+        try:
+            reaction, _ = await self.bot.wait_for('reaction_add', timeout=10.0, check=check)
+        except asyncio.TimeoutError:
+            await ctx.send("Time's Up! :stopwatch:")
+        else:
+            await ctx.send(f"**YOU:\t{reaction.emoji}\nME:\t{bot_emoji}**")
+            # if conds
+            if str(reaction.emoji) == bot_emoji:
+                embed1 = discord.Embed(title=f"Games", description="**It's a Tie :ribbon:**",
+                                       color=discord.Color.gold)
+
+                await ctx.send(embed=embed1)
+
+            elif check_win(str(reaction.emoji), bot_emoji):
+
+                embed2 = discord.Embed(title=f"Games", description="**You win :sparkles:**",
+                                       color=discord.Color.green())
+
+                await ctx.send(embed=embed2)
+
+            else:
+
+                embed3 = discord.Embed(title=f"Games", description="**I win :robot:**",
+                                       color=discord.Color.red())
+
+                await ctx.send(embed=embed3)
+    """
+
     @commands.command(help="Roll dice!", name="Dice", aliases=["rollingdice", "diceroll", "rolldice"])
+    @commands.cooldown(rate=1, per=10.0, type=commands.BucketType.user)
     async def rolldice(self, ctx):
         """Roll dice!"""
         embed1 = discord.Embed(title=f"Games", description="Rolled a {}!".format(random.randint(1, 6)),
@@ -16,13 +78,14 @@ class Games(commands.Cog, description="Game commands"):
         await ctx.send(embed=embed1)
 
     @commands.command(aliases=["8ba"], name="8Ball")
+    @commands.cooldown(rate=1, per=10.0, type=commands.BucketType.user)
     async def eightball(self, ctx, *, question: commands.clean_content):
         """ Consult 8ball to receive an answer """
         ballresponse = [
             "Yes", "No", "Take a wild guess...", "Very doubtful",
             "Sure", "Without a doubt", "Most likely", "Might be possible",
             "You'll be the judge", "no... (╯°□°）╯︵ ┻━┻", "no... baka",
-            "senpai, pls no ;-;"
+            "senpai, pls no ;-;", "I have no idea"
         ]
 
         answer = random.choice(ballresponse)
