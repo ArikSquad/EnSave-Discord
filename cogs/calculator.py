61f1ff42ca6ead479c6a0622e83f0f4b5156fb44
Ari <artturi.kuula2@gmail.com>
2021-11-20 16:49:12 +0200

    Fixed style.

--- cogs/calculator.py
+++ cogs/calculator.py
@@ -25,11 +25,10 @@ class NumericStringParser(object):
     """
     Most of this code comes from the fourFn.py pyparsing example
     """
-
-    def pushFirst(self, strg, loc, toks):
+    def pushfirst(self, strg, loc, toks):
         self.exprStack.append(toks[0])
 
-    def pushUMinus(self, strg, loc, toks):
+    def pushuminus(self, strg, loc, toks):
         if toks and toks[0] == "-":
             self.exprStack.append("unary -")
 
@@ -62,16 +61,16 @@ class NumericStringParser(object):
         pi = CaselessLiteral("PI")
         expr = Forward()
         atom = (
-            (Optional(oneOf("- +")) + (pi | e | fnumber | ident + lpar + expr + rpar).setParseAction(self.pushFirst))
+            (Optional(oneOf("- +")) + (pi | e | fnumber | ident + lpar + expr + rpar).setParseAction(self.pushfirst))
             | Optional(oneOf("- +")) + Group(lpar + expr + rpar)
-        ).setParseAction(self.pushUMinus)
+        ).setParseAction(self.pushuminus)
         # by defining exponentiation as "atom [ ^ factor ]..." instead of
         # "atom [ ^ atom ]...", we get right-to-left exponents, instead of left-to-right
         # that is, 2^3^2 = 2^(3^2), not (2^3)^2.
         factor = Forward()
-        factor << atom + ZeroOrMore((expop + factor).setParseAction(self.pushFirst))
-        term = factor + ZeroOrMore((multop + factor).setParseAction(self.pushFirst))
-        expr << term + ZeroOrMore((addop + term).setParseAction(self.pushFirst))
+        factor << atom + ZeroOrMore((expop + factor).setParseAction(self.pushfirst))
+        term = factor + ZeroOrMore((multop + factor).setParseAction(self.pushfirst))
+        expr << term + ZeroOrMore((addop + term).setParseAction(self.pushfirst))
         # addop_term = ( addop + term ).setParseAction( self.pushFirst )
         # general_term = term + ZeroOrMore( addop_term ) | OneOrMore( addop_term)
         # expr <<  general_term
@@ -100,29 +99,30 @@ class NumericStringParser(object):
             "ceil": math.ceil,
         }
 
-    def evaluateStack(self, s):
+    def evaluatestack(self, s):
         op = s.pop()
         if op == "unary -":
-            return -self.evaluateStack(s)
+            return -self.evaluatestack(s)
         if op in "+-x/^":
-            op2 = self.evaluateStack(s)
-            op1 = self.evaluateStack(s)
+            op2 = self.evaluatestack(s)
+            op1 = self.evaluatestack(s)
             return self.opn[op](op1, op2)
         elif op == "PI":
             return math.pi  # 3.1415926535
         elif op == "E":
             return math.e  # 2.718281828
         elif op in self.fn:
-            return self.fn[op](self.evaluateStack(s))
+            return self.fn[op](self.evaluatestack(s))
         elif op[0].isalpha():
             return 0
         else:
             return float(op)
 
-    def eval(self, num_string, parseAll=True):
+    def eval(self, num_string, parseall=True):
         self.exprStack = []
-        results = self.bnf.parseString(num_string, parseAll)
-        val = self.evaluateStack(self.exprStack[:])
+        results = self.bnf.parseString(num_string, parseall)
+        val = self.evaluatestack(self.exprStack[:])
+        print(results)
         return val
 
 
