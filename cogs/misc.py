f82e3812c93ffb547ca49e2a348fe6ad23cbf669
Ari k <artturi.kuula2@gmail.com>
2022-02-15 17:01:49 +0200

    Renamed the functions again.

--- cogs/misc.py
+++ cogs/misc.py
@@ -20,8 +20,8 @@ class Misc(commands.Cog, description="Misc Commands"):
     def __init__(self, bot):
         self.bot = bot
 
-    @nextcord.slash_command(name="dog", guild_ids=guild_ids, description="Dog pictures!")
-    async def _dog(self, interaction: Interaction):
+    @nextcord.slash_command(name="dog", guild_ids=guild_ids, description="Posts a fun dog picture in the chat!")
+    async def dog(self, interaction: Interaction):
         async with aiohttp.ClientSession() as cs:
             async with cs.get('https://random.dog/woof.json') as r:
                 data = await r.json()
@@ -32,14 +32,14 @@ class Misc(commands.Cog, description="Misc Commands"):
                 embed.set_image(url=data['url'])
                 await interaction.send(embed=embed)
 
-    @nextcord.slash_command(name="discord-invite", guild_ids=guild_ids, description='Get the invite link of the bot.')
-    async def _invite_discord(self, interaction: Interaction):
+    @nextcord.slash_command(name="bot-invite", guild_ids=guild_ids, description='Get the invite link of the bot.')
+    async def bot_invite(self, interaction: Interaction):
         embed = nextcord.Embed(
-            title="Info",
-            description=f"Invite me by clicking"
-                        f" [here](https://discord.com/api/oauth2/userize?"
-                        f"client_id=812808865728954399&permissions=8&redirect_uri="
-                        f"http%3A%2F%2Fdiscord.mikart.eu%2F&scope=bot%20applications.commands).",
+            title='Info',
+            description=f'Invite me by clicking '
+                        f'[here](https://discord.com/api/oauth2/userize?'
+                        f'client_id=812808865728954399&permissions=8&redirect_uri='
+                        f'http%3A%2F%2Fdiscord.mikart.eu%2F&scope=bot%20applications.commands).',
             color=0xD75BF4
         )
         await interaction.send(embed=embed)
