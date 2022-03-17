ec3d84e84be91ac895b7bd57241b82fa6310be53
Ari k <artturi.kuula2@gmail.com>
2022-03-17 17:43:12 +0200

    CHANGES: move files, change functions, discord.py, asyncio updates...

--- main.py
+++ main.py
@@ -7,14 +7,12 @@
 # -----------------------------------------------------------
 
 # The libraries that are for the bot.
-import datetime
-import json
+import asyncio
 import os
 
-import nextcord
-from danbot_api import DanBotClient
+import discord
+from discord.ext import commands
 from dotenv import load_dotenv
-from nextcord.ext import commands
 
 # Coloured text from logger file and Prefix from database file.
 from utils import logger, database
@@ -23,12 +21,10 @@ from utils import logger, database
 # Example: TOKEN='(your token)'
 load_dotenv()
 token = os.getenv('TOKEN')
-danbot = os.getenv('DBHAPI')
-
 
 # Create the activity for Discord. Idle looks cool.
-activity = nextcord.Activity(type=nextcord.ActivityType.watching,
-                             name=f'24/7', status=nextcord.Status.idle)
+activity = discord.Activity(type=discord.ActivityType.watching,
+                            name=f'24/7', status=discord.Status.idle)
 
 # Selects all intents and prefix, case-insensitive, description.
 bot = commands.Bot(command_prefix=database.get_prefix,
@@ -36,90 +32,24 @@ bot = commands.Bot(command_prefix=database.get_prefix,
                    description="EnSave is a Discord bot, that adds some cool utility commands and some music commands. "
                                "Have fun and explore the commands of EnSave.",
                    activity=activity,
-                   status=nextcord.Status.idle,
-                   intents=nextcord.Intents.all())
+                   status=discord.Status.idle,
+                   intents=discord.Intents.all())
 
 
-# This event will be run after bot joins a guild.
-@bot.event
-async def on_guild_join(guild):
-    # Opens the prefixes.json to read the prefixes.
-    with open('db/prefixes.json', 'r') as f:
-        prefixes = json.load(f)
+# Load all the cogs, then print the cog names that have been loaded.
+async def load_cogs():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            loader = filename[:-3]
+            await bot.load_extension(f'cogs.{loader}')
+            logger.print_color(f'{loader.capitalize()} has been loaded', 'HEADER')
+asyncio.run(load_cogs())
 
-    # This is the default prefix. You can change it if you want to!
-    prefixes[str(guild.id)] = '.'
-    # Opens the prefixes.json to write the prefixes.
-    with open('db/prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent=4)
-
-
-# This even will be run after the bot leaves a guild.
-@bot.event
-async def on_guild_remove(guild):
-    # This command just removed the prefix from the database.
-    with open('db/prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-    prefixes.pop(str(guild.id))
-    with open('db/prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent=4)
-
-
-# This is a command to change the prefix.
-@bot.command(name="prefix", help="Change the prefix of the guild.",
-             aliases=["changeprefix"])
-@commands.has_permissions(administrator=True)
-async def change_prefix(ctx, prefix):
-    embed = nextcord.Embed(title="Moderation",
-                           description=f"Changed the prefix to: " + prefix,
-                           color=nextcord.Color.gold())
-    await ctx.send(embed=embed)
-
-    # Opens the prefixes.json to read the prefixes.
-    with open('db/prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-    prefixes[str(ctx.guild.id)] = prefix
-
-    # Writes the prefixes back to the file
-    with open('db/prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent=4)
-
-    print(f'Changed prefix in {ctx.guild} to {prefix}. Command was ran user {ctx.message.author}.')
-
-
-# This will be run, after the bot is ready.
-@bot.event
-async def on_ready():
-    # Send information to DanBot.
-    DanBotClient(bot, key=danbot, autopost=True)
-    # Print some information about the bot.
-    print("Logging in... at the time of " + str(datetime.datetime.now()))
-    print(f'{logger.WARNING}{bot.user} has connected to Discord!{logger.END}')
-    print(f"Name: {logger.CYAN}{bot.user.name}{logger.END}")
-    print(f"ID: {logger.CYAN}{bot.user.id}{logger.END}")
-    print(f'{logger.BOLD}###########################################{logger.END}')
-    print(f"Connected to {logger.GREEN}{len(bot.guilds)} guilds{logger.END}")
-
-    with open("db/users.json", "r") as f:
-        data = json.load(f)
-    guild = bot.get_guild(770634445370687519)
-    async for member in guild.fetch_members():
-        if str(member.id) not in data and not member.bot:
-            data[str(member.id)] = {}
-            data[str(member.id)]["experience"] = 0
-            data[str(member.id)]["level"] = 0
-            data[str(member.id)]["sent"] = 1
-    with open("db/users.json", 'w') as f:
-        json.dump(data, f, indent=4)
-
-
-# Load all the cogs, then tell what have been loaded.
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        loader = filename[:-3]
-        bot.load_extension(f'cogs.{loader}')
-        print(f'{logger.HEADER}{loader.capitalize()} has been loaded{logger.END}')
 
 # Run the bot with token (.env file)
+async def main():
+    async with bot:
+        await bot.start(token, reconnect=True)
+
 if __name__ == "__main__":
-    bot.run(token, reconnect=True)
+    asyncio.run(main())
