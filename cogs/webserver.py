ec6cb664f6abc1ed5fec97013cef50fe5b1ab986
Ari k <artturi.kuula2@gmail.com>
2022-03-18 18:47:30 +0200

    ADDITIONS: added a webserver

---
+++ cogs/webserver.py
@@ -0,0 +1,41 @@
+# -----------------------------------------------------------
+# This is a discord bot by ArikSquad and you are viewing the source code of it.
+#
+# (C) 2022 MikArt
+# Released under the CC BY-NC 4.0 (BY-NC 4.0)
+#
+# -----------------------------------------------------------
+
+from aiohttp import web
+from discord.ext import commands, tasks
+
+app = web.Application()
+routes = web.RouteTableDef()
+
+
+class Webserver(commands.Cog, description="Webserver to handle pings."):
+    def __init__(self, bot):
+        self.bot = bot
+        self.web_server.start()
+
+        @routes.get('/')
+        async def welcome(request):
+            return web.Response(text="Hello, world")
+
+        self.webserver_port = 5000
+        app.add_routes(routes)
+
+    @tasks.loop()
+    async def web_server(self):
+        runner = web.AppRunner(app)
+        await runner.setup()
+        site = web.TCPSite(runner, host='0.0.0.0', port=self.webserver_port)
+        await site.start()
+
+    @web_server.before_loop
+    async def web_server_before_loop(self):
+        await self.bot.wait_until_ready()
+
+
+async def setup(bot):
+    await bot.add_cog(Webserver(bot))
