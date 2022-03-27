14a64bd7612125c9ddc80e0da9f380c1ed993e8a
Ari k <artturi.kuula2@gmail.com>
2022-03-27 11:13:11 +0300

    CHANGES: small rewrite

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -43,17 +43,22 @@ class Welcome(commands.Cog, description="Welcome commands."):
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        try:
-            with open("db/config.json", "r") as f:
-                data = json.load(f)
-                welcome_channel = data[str(member.guild.id)]["welcome_channel"]
-                toggle = data[str(member.guild.id)]["welcome_toggle"]
-        except KeyError:
+        toggle = None
+        welcome_channel = None
+        with open("db/config.json", "r") as f:
+            data = json.load(f)
+
+        if data[str(member.guild.id)] in data:
+            welcome_channel = data[str(member.guild.id)]["welcome_channel"]
+            toggle = data[str(member.guild.id)]["welcome_toggle"]
+        else:
             with open("db/config.json", "r") as f:
                 data = json.load(f)
             data[str(member.guild.id)]["welcome_channel"] = None
+            data[str(member.guild.id)]["welcome_toggle"] = True
             with open("db/config.json", "w") as f:
                 json.dump(data, f, indent=4)
+
         if toggle:
             try:
                 get_channel = discord.utils.get(member.guild.channels, name="ensave-guard")
@@ -61,23 +66,24 @@ class Welcome(commands.Cog, description="Welcome commands."):
                 channel = self.bot.get_channel(channel_id)
             except AttributeError:
                 channel = self.bot.get_channel(welcome_channel)
+
             if member.id == 537237654207725568:
                 return
             else:
                 await channel.send(f"{member.mention} has left {member.guild.name}! super sad :(")
 
-    @commands.command(name="welcome-channel")
+    @commands.command(name="welcome-channel", help="Set the welcome channel to a discord channel.")
     @commands.has_permissions(manage_guild=True)
     async def welcome_channel(self, ctx, channel: discord.TextChannel = None):
         if channel is not None:
-            try:
-                with open("db/config.json", "r") as f:
-                    data = json.load(f)
+            with open("db/config.json", "r") as f:
+                data = json.load(f)
+            if str(channel.id) in data:
                 data[str(ctx.guild.id)]["welcome_channel"] = channel.id
                 await ctx.send(f"Welcome channel set to {channel.mention}.")
                 with open("db/config.json", "w") as f:
                     json.dump(data, f, indent=4)
-            except KeyError:
+            else:
                 with open("db/config.json", "r") as f:
                     data = json.load(f)
                 data[str(ctx.guild.id)] = {}
@@ -85,30 +91,30 @@ class Welcome(commands.Cog, description="Welcome commands."):
                 with open("db/config.json", "w") as f:
                     json.dump(data, f, indent=4)
         else:
-            try:
-                with open("db/config.json", "r") as f:
-                    data = json.load(f)
+            with open("db/config.json", "r") as f:
+                data = json.load(f)
+            if str(ctx.guild.id) in data:
                 channel_id = data[str(ctx.guild.id)]["welcome_channel"]
                 channel = self.bot.get_channel(channel_id)
                 await ctx.send(f"Welcome channel is currently set to {channel.mention}.")
-            except KeyError:
+            else:
                 await ctx.send("Welcome channel is currently set to None.")
 
-    @commands.command(name="welcome-toggle")
+    @commands.command(name="welcome-toggle", help="Toggles welcome messages.")
     @commands.has_permissions(manage_guild=True)
-    async def welcome_toggle(self, ctx):
-        try:
-            with open("db/config.json", "r") as f:
-                data = json.load(f)
-            data[str(ctx.guild.id)]["welcome_toggle"] = not data[str(ctx.guild.id)]["welcome_toggle"]
+    async def welcome_toggle(self, ctx, toggle: bool = None):
+        with open("db/config.json", "r") as f:
+            data = json.load(f)
+        if str(ctx.guild.id) in data:
+            welcome_toggle = data[str(ctx.guild.id)]["welcome_toggle"]
+            data[str(ctx.guild.id)]["welcome_toggle"] = not welcome_toggle if not toggle else toggle
             await ctx.send(f"Welcome toggle set to {data[str(ctx.guild.id)]['welcome_toggle']}.")
             with open("db/config.json", "w") as f:
                 json.dump(data, f, indent=4)
-        except KeyError:
-            with open("db/config.json", "r") as f:
-                data = json.load(f)
+        else:
             data[str(ctx.guild.id)] = {}
             data[str(ctx.guild.id)]["welcome_toggle"] = True
+
             with open("db/config.json", "w") as f:
                 json.dump(data, f, indent=4)
 
