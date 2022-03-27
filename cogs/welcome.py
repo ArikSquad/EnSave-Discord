470c255ca38fc99a525fd7212d46c948807957bb
Ari k <artturi.kuula2@gmail.com>
2022-03-27 10:14:42 +0300

    ADDITIONS: welcome the new users

---
+++ cogs/welcome.py
@@ -0,0 +1,107 @@
+# -----------------------------------------------------------
+# This is a discord bot by ArikSquad and you are viewing the source code of it.
+#
+# (C) 2022 MikArt
+# Released under the CC BY-NC 4.0 (BY-NC 4.0)
+#
+# -----------------------------------------------------------
+import json
+
+import discord
+from discord.ext import commands
+
+
+class Welcome(commands.Cog, description="Welcome commands."):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        try:
+            with open("db/config.json", "r") as f:
+                data = json.load(f)
+                welcome_channel = data[str(member.guild.id)]["welcome_channel"]
+                toggle = data[str(member.guild.id)]["welcome_toggle"]
+        except KeyError:
+            with open("db/config.json", "r") as f:
+                data = json.load(f)
+            data[str(member.guild.id)]["welcome_channel"] = None
+            with open("db/config.json", "w") as f:
+                json.dump(data, f, indent=4)
+        if toggle:
+            try:
+                get_channel = discord.utils.get(member.guild.channels, name="ensave-guard")
+                channel_id = get_channel.id
+                channel = self.bot.get_channel(channel_id)
+            except AttributeError:
+                channel = self.bot.get_channel(welcome_channel)
+            if member.id == 537237654207725568:
+                return
+            else:
+                await channel.send(f"Welcome {member.mention} to {member.guild.name}! "
+                                   f"We have a Minecraft server play.mikart.eu!")
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        try:
+            with open("db/config.json", "r") as f:
+                data = json.load(f)
+                welcome_channel = data[str(member.guild.id)]["welcome_channel"]
+                toggle = data[str(member.guild.id)]["welcome_toggle"]
+        except KeyError:
+            with open("db/config.json", "r") as f:
+                data = json.load(f)
+            data[str(member.guild.id)]["welcome_channel"] = None
+            with open("db/config.json", "w") as f:
+                json.dump(data, f, indent=4)
+        if toggle:
+            try:
+                get_channel = discord.utils.get(member.guild.channels, name="ensave-guard")
+                channel_id = get_channel.id
+                channel = self.bot.get_channel(channel_id)
+            except AttributeError:
+                channel = self.bot.get_channel(welcome_channel)
+            if member.id == 537237654207725568:
+                return
+            else:
+                await channel.send(f"{member.mention} has left {member.guild.name}! super sad :(")
+
+    @commands.command(name="welcome-channel")
+    @commands.has_permissions(manage_guild=True)
+    async def welcome_channel(self, ctx, channel: discord.TextChannel):
+        try:
+            with open("db/config.json", "r") as f:
+                data = json.load(f)
+            data[str(ctx.guild.id)]["welcome_channel"] = channel.id
+            await ctx.send(f"Welcome channel set to {channel.mention}.")
+            with open("db/config.json", "w") as f:
+                json.dump(data, f, indent=4)
+        except KeyError:
+            with open("db/config.json", "r") as f:
+                data = json.load(f)
+            data[str(ctx.guild.id)] = {}
+            data[str(ctx.guild.id)]["welcome_channel"] = channel.id if channel else None
+            with open("db/config.json", "w") as f:
+                json.dump(data, f, indent=4)
+
+    @commands.command(name="welcome-toggle")
+    @commands.has_permissions(manage_guild=True)
+    async def welcome_toggle(self, ctx):
+        try:
+            with open("db/config.json", "r") as f:
+                data = json.load(f)
+            data[str(ctx.guild.id)]["welcome_toggle"] = not data[str(ctx.guild.id)]["welcome_toggle"]
+            await ctx.send(f"Welcome toggle set to {data[str(ctx.guild.id)]['welcome_toggle']}.")
+            with open("db/config.json", "w") as f:
+                json.dump(data, f, indent=4)
+        except KeyError:
+            with open("db/config.json", "r") as f:
+                data = json.load(f)
+            data[str(ctx.guild.id)] = {}
+            data[str(ctx.guild.id)]["welcome_toggle"] = True
+            with open("db/config.json", "w") as f:
+                json.dump(data, f, indent=4)
+
+
+async def setup(bot):
+    await bot.add_cog(Welcome(bot))
