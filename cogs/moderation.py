ceec1b499d1eb6908dd45368e0f86088580a11dc
Ari k <artturi.kuula2@gmail.com>
2022-04-04 19:26:39 +0300

    REMOVE: useless command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -56,42 +56,6 @@ class Moderation(commands.Cog, description="Moderation commands.."):
                               color=0x00ff00)
         await ctx.send(embed=embed)
 
-    # This command was forked from
-    # https://github.com/Carberra/updated-discord.py-tutorial/blob/085113e9bff69a699a25ed1cd91db5744b8755ea/lib/cogs/info.py#L37
-    @commands.command(name="serverinfo", aliases=["guildinfo", "si", "gi"])
-    @commands.has_permissions(administrator=True)
-    async def server_info(self, ctx):
-        embed = discord.Embed(title="Server information",
-                              colour=ctx.guild.owner.colour,
-                              timestamp=datetime.datetime.utcnow())
-
-        embed.set_thumbnail(url=ctx.guild.icon.url)
-
-        statuses = [len(list(filter(lambda m: str(m.status) == "online", ctx.guild.members))),
-                    len(list(filter(lambda m: str(m.status) == "idle", ctx.guild.members))),
-                    len(list(filter(lambda m: str(m.status) == "dnd", ctx.guild.members))),
-                    len(list(filter(lambda m: str(m.status) == "offline", ctx.guild.members)))]
-
-        fields = [("ID", ctx.guild.id, True),
-                  ("Owner", ctx.guild.owner, True),
-                  ("Created at", ctx.guild.created_at.strftime("%d/%m/%Y %H:%M:%S"), True),
-                  ("Members", len(ctx.guild.members), True),
-                  ("Users", len(list(filter(lambda m: not m.bot, ctx.guild.members))), True),
-                  ("Bots", len(list(filter(lambda m: m.bot, ctx.guild.members))), True),
-                  ("Banned members", len(await ctx.guild.bans()), True),
-                  ("Statuses", f"🟢 {statuses[0]} 🟠 {statuses[1]} \n🔴 {statuses[2]} ⚪ {statuses[3]}", True),
-                  ("Text channels", len(ctx.guild.text_channels), True),
-                  ("Voice channels", len(ctx.guild.voice_channels), True),
-                  ("Categories", len(ctx.guild.categories), True),
-                  ("Roles", len(ctx.guild.roles), True),
-                  ("Invites", len(await ctx.guild.invites()), True),
-                  ("\u200b", "\u200b", True)]
-
-        for name, value, inline in fields:
-            embed.add_field(name=name, value=value, inline=inline)
-
-        await ctx.send(embed=embed)
-
     @commands.command(name="prefix", help="Change the prefix of the guild.",
                       aliases=["changeprefix"])
     @commands.has_permissions(administrator=True)
