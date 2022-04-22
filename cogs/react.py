749e1da0087314c7b3d71e656430182cdd21d7ee
Ari k <artturi.kuula2@gmail.com>
2022-04-22 17:42:07 +0300

    CHANGES: update.py changed to react.py

--- cogs/react.py
+++ cogs/react.py
@@ -5,28 +5,14 @@
 # Released under the CC BY-NC 4.0 (BY-NC 4.0)
 #
 # -----------------------------------------------------------
-
 from discord.ext import commands
-from git import Repo
-
-from utils import database
 
 
-async def clone(bot):
-    Repo.clone_from('https://github.com/ArikSquad/EnSave-Discord', '../')
-    await bot.close()
-
-
-class Git(commands.Cog, description="Github"):
+class React(commands.Cog, description="React to messages"):
     EMOJI = "🎈"
 
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command(name="pull", hidden=True)
-    async def pull(self, ctx):
-        if ctx.author.id in database.get_owner_ids():
-            await clone(self.bot)
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -34,8 +20,7 @@ class Git(commands.Cog, description="Github"):
             if message.author.id == 962313982746714142:
                 emoji = self.bot.get_emoji(854963304227930123)
                 await message.add_reaction(emoji)
-                await clone(self.bot)
 
 
 async def setup(bot):
-    await bot.add_cog(Git(bot))
+    await bot.add_cog(React(bot))
