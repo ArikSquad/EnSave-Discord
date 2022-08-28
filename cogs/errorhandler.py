628884fc936a9161e11ee74d6867c56a7172170a
Ari k <75741608+ArikSquad@users.noreply.github.com>
2022-08-28 12:40:54 +0300

    CHANGES: errorhandler for slash commands

--- cogs/errorhandler.py
+++ cogs/errorhandler.py
@@ -25,21 +25,19 @@ def error_formatter(error, maxlength):
 
 # This cog will send the message to the owner, when an error has ocurred.
 class Errorhandler(commands.Cog, description="Errorhandler"):
-    EMOJI = "❌"
-
     def __init__(self, bot):
         self.bot = bot
+        bot.tree.on_error = self.on_app_command_error
 
-    @staticmethod
-    async def traceback(ctx: commands.Context, embed: discord.Embed):
-        app_info = await ctx.bot.application_info()
+    async def traceback(self, interaction: discord.Interaction, embed: discord.Embed):
+        app_info = await self.bot.application_info()
 
         channel = app_info.owner.dm_channel if app_info.owner.dm_channel else await app_info.owner.create_dm()
 
         if not channel:
             return
 
-        perms = channel.permissions_for(ctx.guild.me if ctx.guild else None)
+        perms = channel.permissions_for(interaction.guild.me if interaction.guild else None)
         if not perms.view_channel or not perms.send_messages or not perms.embed_links:
             return
 
@@ -52,23 +50,19 @@ class Errorhandler(commands.Cog, description="Errorhandler"):
         commands.CheckFailure: '{err}',
         commands.CommandOnCooldown: '{err}',
         commands.errors.CommandOnCooldown: '{err}',
-        commands.MissingRequiredArgument: 'You are missing a required argument! '
-                                          '(See `{ctx.prefix}help {ctx.command.qualified_name}` '
-                                          'for info on how to use this command).',
-        commands.BadArgument: 'Invalid argument given! (See `{ctx.prefix}help {ctx.command.qualified_name}` '
-                              'for info on how to use this command).',
+        commands.MissingRequiredArgument: 'You are missing a required argument! ',
+        commands.BadArgument: 'Invalid argument given!',
         commands.CommandNotFound: None,
-        commands.UserInputError: 'Invalid command usage! (See `{ctx.prefix}help {ctx.command.qualified_name}` ',
-        aiohttp.ContentTypeError: 'Something went wrong. This usually happens when you use music commands.',
+        commands.UserInputError: 'Invalid command usage!',
+        aiohttp.ClientConnectorError: 'The music server is currently offline.',
     }
 
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx: commands.Context, error):
+    async def on_app_command_error(self, interaction: discord.Interaction, error):
         classname = error.__class__
         if classname in self._error_messages.keys():
             msg = self._error_messages.get(classname, None)
             if isinstance(msg, str):
-                await ctx.send(msg.format(ctx=ctx, err=error))
+                await interaction.response.send_message(msg.format(interaction=interaction, err=error))
         else:
 
             if isinstance(error, (commands.CommandError, commands.CheckFailure)) \
@@ -78,11 +72,11 @@ class Errorhandler(commands.Cog, description="Errorhandler"):
                     description=f'{error}',
                     color=discord.Color.red()
                 )
-                error_embed.add_field(name='Command Information', value=f'Command: {ctx.command}\n'
-                                                                        f'Description: {ctx.command.help}\n'
-                                                                        f'Cog: {ctx.command.cog.qualified_name}\n')
-                error_embed.set_footer(text=f'You can also try .help {ctx.command.qualified_name}')
-                return await ctx.send(embed=error_embed)
+                error_embed.add_field(name='Command Information',
+                                      value=f'Command: {interaction.command}\n'
+                                            f'Description: {interaction.command.description}\n'
+                                            f'Cog: {interaction.command.cog.qualified_name}\n')
+                return await interaction.response.send_message(embed=error_embed)
 
             execute_error = discord.Embed(
                 color=discord.Colour(0xff0000),
@@ -91,7 +85,7 @@ class Errorhandler(commands.Cog, description="Errorhandler"):
                 timestamp=datetime.datetime.utcnow()
             )
 
-            await ctx.send(embed=execute_error)
+            await interaction.response.send_message(embed=execute_error)
 
             print(error.original)
 
@@ -101,19 +95,20 @@ class Errorhandler(commands.Cog, description="Errorhandler"):
                 timestamp=datetime.datetime.utcnow()
             )
 
-            embed.add_field(name='Command', value=ctx.command)
+            embed.add_field(name='Command', value=interaction.command)
             embed.add_field(name='Original message',
-                            value=ctx.message.content[:1021] + (ctx.message.content[1021:] and '...'))
+                            value=interaction.message.content[:1021] + (interaction.message.content[1021:] and '...'))
             embed.add_field(name='Channel',
-                            value='Private Message' if isinstance(ctx.channel,
-                                                                  discord.DMChannel) else f'#{ctx.channel.name}'
-                                                                                          f' (`{ctx.channel.id}`)')
-            embed.add_field(name='Sender', value=f'{ctx.author} (`{ctx.author.id}`)')
+                            value='Private Message' if isinstance(interaction.channel,
+                                                                  discord.DMChannel)
+                            else f'#{interaction.channel.name}'
+                                 f' (`{interaction.channel.id}`)')
+            embed.add_field(name='Sender', value=f'{interaction.user} (`{interaction.user.id}`)')
             embed.add_field(name='Exception', value=str(error))
             formatted_traceback = error_formatter(error.original, 4094)
-            embed.description = f'```py\nCog {ctx.cog.qualified_name}\n{formatted_traceback}```'
+            embed.description = f'```py\nCog {interaction.command.cog.qualified_name}\n{formatted_traceback}```'
 
-            await self.traceback(ctx, embed)
+            await self.traceback(interaction, embed)
 
 
 async def setup(bot):
