589fe4f91c32b3bbf0056014b174d4f07e32740b
Ari k <75741608+ArikSquad@users.noreply.github.com>
2023-01-01 19:17:26 +0200

    useless file

--- __init__.py
+++
@@ -1,7 +0,0 @@
-# -----------------------------------------------------------
-# This is a discord bot by ArikSquad and you are viewing the source code of it.
-#
-# (C) 2021-2022 MikArt
-# Released under the Apache License 2.0
-#
-# -----------------------------------------------------------
